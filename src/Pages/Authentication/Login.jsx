1152869292c63ef92d7c7eec968904777b0b1f0f
anusraza268 <anusraza268@gmail.com>
2025-11-02 06:56:36 +0000

    setting app

---
+++ src/Pages/Authentication/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+<>
+<section>
+  <div className='login '>
+    <h1 className='text-lg font-bold'>Saad Hussain Rizvi</h1>
+
+  </div>
+</section>
+</>
+
+
+  )
+}
+
+export default Login
